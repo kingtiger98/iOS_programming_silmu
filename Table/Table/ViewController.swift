bc6c9a7b464b47559e3e884d077a3c5c14ed68f7
Jaeha Hwang <wogk216@naver.com>
2023-04-12 12:56:21 +0900

    테이블뷰 실습

--- Table/Table/ViewController.swift
+++ Table/Table/ViewController.swift
@@ -12,22 +12,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
-        
-        
-        
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    // section 수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
     }
     
     // section에서 row 수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 3
     }
     
     // cell 생성
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell.init(style: .default, reuseIdentifier: "myCell")
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableViewCell {
+        // MyTableViewCell의 인스턴스에 접근하기 위해서 다운캐스팅 함 : as?
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as? MyTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.myLabel.text = "재하"
         
+        return cell
     }
-    
 }
