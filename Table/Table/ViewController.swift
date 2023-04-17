4f3a076445b1dbf6d85a137ba918bc01382339b1
Jaeha Hwang <wogk216@naver.com>
2023-04-17 17:15:03 +0900

    화면전환방식_present_dismiss

--- Table/Table/ViewController.swift
+++ Table/Table/ViewController.swift
@@ -18,6 +18,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
     }
     
+    // 뷰 이동 소스 : present()
+    @IBAction func presentBtn(_ sender: Any) {
+        // 스토리보드 초기화
+        let SecondVC = storyboard?.instantiateViewController(identifier: "SecondVC")
+        // 트랜지션 방법 지정
+        SecondVC?.modalPresentationStyle = .automatic
+        // present사용!
+        present(SecondVC!, animated: true)
+    }
+    
+    
+    
     // section 수
     func numberOfSections(in tableView: UITableView) -> Int {
         return 3
@@ -25,7 +37,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // section에서 row 수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 2
     }
     
     // cell 생성
@@ -35,7 +47,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as? MyTableViewCell else {
             return UITableViewCell()
         }
-        cell.myLabel.text = "재하"
+        cell.myLabel.text = "첫 번째 테이블"
         return cell
     }
 }
